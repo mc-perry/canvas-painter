adc8dd71c783b9e69942a079163262171d22f349
Jonathan Perry <jpper@umich.edu>
2019-12-04 12:53:54 +0900

    When I should stop watching the tutorial (thanks Simon) and finish it myself

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from "@angular/core";
+import { Platform } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild("imageCanvas", { static: false }) canvas: any;
+  canvasElement: any;
+  saveX: number;
+  saveY: number;
+  drawing = false;
 
-  constructor() {}
+  selectedColor = "#9e2956";
+  colors = [
+    "#9e2956",
+    "#c2281d",
+    "#de722f",
+    "#edbf4c",
+    "#5db37e",
+    "#459cde",
+    "#4250ad",
+    "#802fa3"
+  ];
+  lineWidth = 5;
 
+  constructor(private plt: Platform) {}
+
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasElement.width = this.plt.width() + "";
+    this.canvasElement.height = 200;
+  }
+
+  startDrawing(ev) {
+    console.log("start: ", ev);
+    this.drawing = true;
+  }
+
+  endDrawing() {
+    console.log("end");
+    this.drawing = false;
+  }
+
+  moved(ev) {
+    if (!this.drawing) return;
+    console.log("moved: ", ev);
+  }
 }
